0f4310c8b4d1c91686699fa0055ed804ccc22af3
Sanya <98276922+Sanya015@users.noreply.github.com>
2024-11-12 21:08:01 +0530

    Simple & Direct solution

---
+++ First Unique Character in a String/solution.cpp
@@ -0,0 +1,16 @@
+// T.C.=O(N) S.C.=O(N)
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n=s.length();
+        unordered_map<char, int> freq;
+        for(int i=0; i<n; i++){
+            freq[s[i]]++;
+        }
+        for(int i=0; i<n; i++){
+            if(freq[s[i]]==1) return i;
+        }
+        return -1;
+    }
+};
