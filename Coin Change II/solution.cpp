bde36c70ad1c7cc4e3ed631d5858ff0858f2d887
Sanya <98276922+Sanya015@users.noreply.github.com>
2025-01-10 15:34:27 +0530

    recursive and dp solutions

--- Coin Change II/solution.cpp
+++ Coin Change II/solution.cpp
@@ -44,4 +44,54 @@ public:
     }
 };
 
-// TABULATION GIVING SOME ISSUES
+// TABULATION T.C.=O(N*M) S.C.=O(N*M)
+
+class Solution {
+public:
+
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<double>> dp(n, vector<double>(amount+1, 0));
+        for(int i=0; i<n; i++){
+            dp[i][0]=1;
+        }
+        for(int i=0; i<=amount; i++){
+            if(i%coins[0]==0) dp[0][i]=1;
+        }
+        for(int i=1; i<n; i++){
+            for(int j=1; j<=amount; j++){
+                double inc=0;
+                if(j>=coins[i]) inc=dp[i][j-coins[i]];
+                double ninc=dp[i-1][j];
+                dp[i][j]=inc+ninc;
+
+            }
+        }
+        return dp[n-1][amount];
+    }
+};
+
+//SPACE OPTIMIZATION T.C.=O(N*M) S.C.=O(M)
+class Solution {
+public:
+
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<double> prev(amount+1, 0);
+        for(int i=0; i<=amount; i++){
+            if(i%coins[0]==0) prev[i]=1;
+        }
+        for(int i=1; i<n; i++){
+            vector<double> curr(amount+1, 0);
+            for(int j=0; j<=amount; j++){
+                double inc=0;
+                if(j>=coins[i]) inc=curr[j-coins[i]];
+                double ninc=prev[j];
+                curr[j]=inc+ninc;
+
+            }
+            prev=curr;
+        }
+        return prev[amount];
+    }
+};
